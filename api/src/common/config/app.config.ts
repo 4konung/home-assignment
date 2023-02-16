3defeeac3ca4d888eaadf32c028c1313b1ecde5f
Vadym Khabibianov <4konung@gmail.com>
2023-02-16 18:18:20 +0200

    adds new app config param

--- api/src/common/config/app.config.ts
+++ api/src/common/config/app.config.ts
@@ -2,6 +2,7 @@ export interface AppConfiguration {
   port: number;
   globalPrefix: string;
   confidenceThreshold: number;
+  environment: string;
 }
 
 export const appConfiguration = (): { app: AppConfiguration } => ({
@@ -9,5 +10,6 @@ export const appConfiguration = (): { app: AppConfiguration } => ({
     globalPrefix: 'api',
     port: parseInt(process.env.API_PORT, 10) || 3333,
     confidenceThreshold: parseInt(process.env.CONFIDENCE_THRESHOLD, 10) || 0,
+    environment: process.env.NODE_ENV,
   },
 });
